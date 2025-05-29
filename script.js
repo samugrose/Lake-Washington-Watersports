250d2b86932fd09abde6b0d9d3a1e084e448cec9
samugrose <samugrose@gmail.com>
2025-05-29 10:43:41 -0700

    added collapsed div with booking calendar, click button to show

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+
+
 $("p").on("click", function () {
     console.log("click click");
 });
+
+/* $("#bookingCalendarButton").on("click", function(){
+  $("#bookingCalendar").css("visibility", "visible");
+  $("#bookingCalendarButton").collapse({
+    toggle : false;
+  });
+  console.log("clicked Div");
+}); */
+
